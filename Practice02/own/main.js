0cf1dc117731a848540971dd230991affc769606
b03902130 <b03902130@ntu.edu.tw>
2019-02-28 01:18:27 +0800

    add gear async loading effect & delete unuse images

--- Practice02/own/main.js
+++ Practice02/own/main.js
@@ -2,12 +2,15 @@ const back = document.getElementById("back");
 const next = document.getElementById("next");
 const image = document.getElementById("display");
 const source = document.getElementById("source");
-const imgnum = parseInt(image.dataset.maxnum);
 
-const urls = [1, 2, 3, 4, 5].map((index) => { 
-    return "images/pizza" + index.toString() + ".jpg";
-});
-
+const urls = [
+    "images/pizza01.jpg",
+    "https://food-images.files.bbci.co.uk/food/recipes/quick_pepperoni_pizza_64616_16x9.jpg",
+    "https://i.imgur.com/AR2SwFPr.jpg",
+    "https://d2gk7xgygi98cy.cloudfront.net/20-4-facebook.jpg",
+    "http://www.luigispizzasub.com/wp-content/uploads/2016/04/Pizza-capricciosa.jpg"
+];
+const imgnum = urls.length;
 
 function clickHandler(e) {
     const target = e.target;
@@ -18,26 +21,35 @@ function clickHandler(e) {
         let index = parseInt(image.dataset.id);
         if (target.id === "back") {
             next.className = "";
-            if (index === 1) {
-                target.className = "disabled";
-                console.log("At left end");
-                return;
+            next.disabled = false;
+            if (index > 1) {
+                index -= 1;
+                if (index === 1) {
+                    target.className = "disabled";
+                    target.disabled = true;
+                }
             }
-            else { index -= 1; }
         }
         else if (target.id === "next") {
             back.className = "";
-            if (index === imgnum) {
-                target.className = "disabled";
-                console.log("At right end");
-                return;
+            back.disabled = false;
+            if (index < imgnum) {
+                index += 1;
+                if (index === imgnum) {
+                    target.className = "disabled";
+                    target.disabled = true;
+                }
             }
-            else { index += 1; }
         }
+        image.src = "images/loading.gif";
+        let downloading = new Image();
+        downloading.onload = function(){
+            image.src = this.src;
+        };
         indexString = index.toString();
         image.dataset.id = indexString;
         const path = urls[index - 1];
-        image.src = path;
+        downloading.src = path;
         source.textContent = path;
     }
 }
