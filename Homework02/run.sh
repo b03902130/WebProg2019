37f8044667e889096aad5f854492121a6afde82f
b03902130 <b03902130@ntu.edu.tw>
2019-03-13 21:33:25 +0800

    finish control flow and basic background

--- Homework02/run.sh
+++ Homework02/run.sh
@@ -1,7 +1,9 @@
-parsedVersion=$(echo "${version//./}")
-if [[ "$parsedVersion" -lt "300" ]]
-then 
-    python server/py2_server.py
-else
-    python3 server/py3_server.py
-fi
+# parsedVersion=$(echo "${version//./}")
+# if [[ "$parsedVersion" -lt "300" ]]
+# then 
+#     python server/py2_server.py
+# else
+#     python3 server/py3_server.py
+# fi
+
+python server/py3_server.py
--- Homework02/sketch.js
+++ Homework02/sketch.js
@@ -1,8 +1,26 @@
 let cvsWrapper = null;
 
 // assets from: https://github.com/sourabhv/FlapPyBird/tree/master/assets
+var stateMode = ["Start", "Play", "End"];
+var state = "Start";
+var score = 0;
 
 function preload() {
+    bg = loadImage(`./assets/sprites/background-${Math.random() < 0.5 ? "day" : "night"}.png`);
+    start = loadImage("./assets/sprites/message.png");
+    digits = [...Array(10).keys()].map((x) => loadImage(`./assets/sprites/${x}.png`));
+    gameover = loadImage("./assets/sprites/gameover.png")
+}
+
+function plotScore(y) {
+    let digitsStr = score.toString().split("");
+    let center = (digitsStr.length + 1) / 2;
+    digitsStr.forEach((digitStr, index) => {
+        image(digits[digitStr], width / 2 + digits[0].width * (index + 1 - center), y);
+    })
+    if (score === 10) {
+        state = "End";
+    }
 }
 
 function setup() {
@@ -16,12 +34,63 @@ function setup() {
     myCanvas.parent("canvasWrapper");
 
     // setup code below
+    bgScrollSpeed = 2;
+    pipeScrollSpeed = 3;
+    bgx1 = 0
+    bgx2 = width;
+}
+
+function scrollBackground() {
+    bgx1 -= bgScrollSpeed;
+    bgx2 -= bgScrollSpeed;
+    if (bgx1 < -width) {
+        bgx1 = width;
+    }
+    if (bgx2 < -width) {
+        bgx2 = width;
+    }
 }
 
 function draw() {
-    // Render function (called per frame.)
-    background(0);
+    // plot background
+    if (state === "Start" || state === "Play") {
+        scrollBackground();
+    }
+    image(bg, bgx1, 0, width, height);
+    image(bg, bgx2, 0, width, height);
+
+    // plot game title
+    if (state === "Start") {
+        imageMode(CENTER);
+        image(start, width / 2, height / 2, width * 0.6, height * 0.6);
+        imageMode(CORNER);
+    }
+
+    // plot score
+    if (state === "Play") {
+        imageMode(CENTER);
+        plotScore(height * 0.2);
+        imageMode(CORNER);
+    }
+
+    if (state === "End") {
+        imageMode(CENTER);
+        image(gameover, width / 2, height * 0.4);
+        plotScore(height * 0.5);
+        imageMode(CORNER);
+    }
 }
 
 function keyPressed() {
+    if (state === "Start") {
+        state = "Play";
+    }
+    else if (state === "Play") {
+        // state = "End";
+        score += 1;
+    }
+    else if (state === "End") {
+        state = "Start";
+        score = 0;
+    }
 }
