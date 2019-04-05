aa2e17f0664226e8674a14bbe71cd1d822de431d
b03902130 <b03902130@ntu.edu.tw>
2019-04-06 06:37:39 +0800

    final one left

--- Practice05/own/src/containers/CalcApp.js
+++ Practice05/own/src/containers/CalcApp.js
@@ -2,30 +2,117 @@ import React from 'react';
 
 import CalcButton from '../components/CalcButton';
 
+const operators = ["+", "-", "x", "÷", "="];
+
 // 計算機 App
 class CalcApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       display: "0",
-      stack: ["0"]
+      operands: ["0"],
+      operator: ""
     };
+    this.editable = true;
+    this.tmp = [];
     this.buttons = [
-      ["AC", "+/-", "%", "÷"],
-      ["7", "8", "9", "x"],
-      ["4", "5", "6", "-"],
-      ["1", "2", "3", "+"],
-      ["0", ".", "="]
+      ["AC", "+/-", "%", operators[3]],
+      ["7", "8", "9", operators[2]],
+      ["4", "5", "6", operators[1]],
+      ["1", "2", "3", operators[0]],
+      ["0", ".", operators[4]]
     ];
   }
 
+  calculate(tmp) {
+    let a = parseInt(this.state.operands[0]);
+    let b = tmp ? this.tmp[1] : parseInt(this.state.operands[1]);
+    let operator = tmp ? this.tmp[0] : this.state.operator;
+    let result;
+    if (operator === operators[0]) {
+      result = a + b;
+    }
+    else if (operator === operators[1]) {
+      result = a - b;
+    }
+    else if (operator === operators[2]) {
+      result = a * b;
+    }
+    else if (operator === operators[3]) {
+      result = Math.floor(a / b);
+    }
+    return result.toString();
+  }
+
   resetState() {
-    // TODO
+    this.setState(state => {
+      return {
+        operands: [this.state.display],
+        operator: ""
+      };
+    });
   }
 
   clickHandler = (e) => {
-    let text = e.target.textContent;
-
+    let input = e.target.textContent;
+    if (input.match(/^[0-9]$/)) {
+      if (this.editable) {
+        this.setState(state => {
+          let lastest = state.operands[state.operands.length - 1];
+          let newDisplay = lastest === "0" ? input : lastest + input;
+          state.display = newDisplay;
+          state.operands[state.operands.length - 1] = newDisplay;
+          return state;
+        });
+      }
+      else {
+        this.editable = true;
+        this.setState({
+          display: input,
+          operands: [input],
+          operator: ""
+        });
+      }
+    }
+    else if (input === "=") {
+      let newDisplay = (this.state.operands.length === 2 && this.state.operands[1] !== "") ? this.calculate(false) : this.state.operands[0];
+      this.editable = false;
+      this.setState({
+        display: newDisplay,
+        operands: [newDisplay],
+        operator: ""
+      });
+    }
+    else if (operators.indexOf(input) !== -1) {
+      if (!this.editable) {
+        this.editable = true;
+      }
+      if (this.state.operands.length === 2) {
+        if (this.state.operands[1] === "") {
+          this.setState({
+            operator: input
+          });
+        }
+        else {
+          let newDisplay = this.calculate(false);
+          this.setState({
+            display: newDisplay,
+            operands: [newDisplay, ""],
+            operator: input
+          });
+        }
+      }
+      else if (this.state.operands.length < 2) {
+        this.setState(state => {
+          state.operands.push("");
+          state.operator = input;
+          return state;
+        });
+      }
+    }
+    else if (input === "AC") {
+      this.resetState();
+    }
   }
 
   buttonRenderer(button, index, length) {
