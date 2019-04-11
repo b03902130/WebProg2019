beda6c6b91ee45aa1fdd2bd5a3f52998966db4fc
b03902130 <b03902130@ntu.edu.tw>
2019-04-11 09:38:16 +0800

    first"

--- Practice06/own/src/containers/Blog/Blog.js
+++ Practice06/own/src/containers/Blog/Blog.js
@@ -1,29 +1,52 @@
 import React, { Component } from "react";
 import { NavLink, Switch, Route, Redirect } from "react-router-dom";
 
+import Home from "./Home"
 import Posts from "./Posts/Posts";
 import PostRender from "./Posts/PostRender";
 
 export default class Blog extends Component {
-    render() {
-        return (
-            <div>
-                <button>
-                    <NavLink to="/home">Home</NavLink>
-                </button>
-                <button>
-                    <NavLink to="/posts">Posts</NavLink>
-                </button>
-                <button>
-                    <NavLink to="/authors">Authors</NavLink>
-                </button>
-                <hr />
-                <Switch>
-                    <Route exact path="/posts" component={Posts} />
-                    <Route path="/posts/:id?" component={PostRender} />
-                    <Redirect from="/home" to="/" />
-                </Switch>
+  render() {
+    return (
+      <div>
+        <header id="header">
+          <NavLink className="logo" to="/home">Home</NavLink>
+          <NavLink className="logo" to="/posts">Posts</NavLink>
+          <NavLink className="logo" to="/authors">Authors</NavLink>
+        </header>
+
+        <Switch>
+          <Route exact path="/posts" component={Posts} />
+          <Route path="/posts/:id?" component={PostRender} />
+          <Route exact path="/" component={Home} />
+          <Redirect from="/home" to="/" />
+        </Switch>
+
+        <footer id="footer">
+          <div class="inner">
+            <div class="content">
+              <section>
+                <h3>CUTE CAT CO.</h3>
+                <p>Many many cute cats</p>
+              </section>
+              <section>
+                <h4>Sem turpis amet semper</h4>
+                <ul class="alt">
+                  <li><a href="#">Dolor pulvinar sed etiam.</a></li>
+                  <li><a href="#">Etiam vel lorem sed amet.</a></li>
+                </ul>
+              </section>
+              <section>
+                <h4>Magna sed ipsum</h4>
+                <ul class="plain">
+                  <li><a href="#"><i class="icon fa-twitter">&nbsp;</i>Twitter</a></li>
+                  <li><a href="#"><i class="icon fa-facebook">&nbsp;</i>Facebook</a></li>
+                </ul>
+              </section>
             </div>
-        );
-    }
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
---
+++ Practice06/own/src/containers/Blog/Home.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+export default class Posts extends Component {
+    render() {
+        return (
+            <div>
+                <section id="banner">
+                    <div class="inner">
+                        <h1>MY CAT WEBSITE</h1>
+                        <p>You can find many cute cats here, without a single dog.<br />No dog sorry.</p>
+                    </div>
+                </section>
+
+                <section class="wrapper">
+                    <div class="inner">
+                        <header class="special">
+                            <h2>Sem turpis amet semper</h2>
+                            <p>In arcu accumsan ar</p>
+                        </header>
+                        <div class="highlights">
+                            <section>
+                                <div class="content">
+                                    <header>
+                                        <a href="#" class="icon fa-vcard-o"><span class="label">Icon</span></a>
+                                        <h3>Feugiat consequat</h3>
+                                    </header>
+                                    <p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttitor sem</p>
+                                </div>
+                            </section>
+                            <section>
+                                <div class="content">
+                                    <header>
+                                        <a href="#" class="icon fa-files-o"><span class="label">Icon</span></a>
+                                        <h3>Ante sem integer</h3>
+                                    </header>
+                                    <p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttitor sem non</p>
+                                </div>
+                            </section>
+                            <section>
+                                <div class="content">
+                                    <header>
+                                        <a href="#" class="icon fa-floppy-o"><span class="label">Icon</span></a>
+                                        <h3>Ipsum consequat</h3>
+                                    </header>
+                                    <p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttitor s</p>
+                                </div>
+                            </section>
+                            <section>
+                                <div class="content">
+                                    <header>
+                                        <a href="#" class="icon fa-line-chart"><span class="label">Icon</span></a>
+                                        <h3>Interdum gravida</h3>
+                                    </header>
+                                    <p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttito</p>
+                                </div>
+                            </section>
+                            <section>
+                                <div class="content">
+                                    <header>
+                                        <a href="#" class="icon fa-paper-plane-o"><span class="label">Icon</span></a>
+                                        <h3>Faucibus consequat</h3>
+                                    </header>
+                                    <p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttitor sem non </p>
+                                </div>
+                            </section>
+                            <section>
+                                <div class="content">
+                                    <header>
+                                        <a href="#" class="icon fa-qrcode"><span class="label">Icon</span></a>
+                                        <h3>Accumsan viverra</h3>
+                                    </header>
+                                    <p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttitor sem non mi int</p>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        );
+    }
+}
